b79b5d19537d9322807b880ff204dfe121df18db
Push <49253201+RideTheSkyP@users.noreply.github.com>
2020-10-31 16:49:04 +0100

    Ex1

---
+++ List2/Ex1/ascending.jl
@@ -0,0 +1,34 @@
+function ascending(type)
+	x = type[2.718281828, -3.141592654, 1.414213562, 0.577215664, 0.301029995]
+	y = type[1486.2497, 878366.9879, -22.37492, 4773714.647, 0.000185049]
+	pos = []
+	neg = []
+	sum = 0
+	value = -1.00657107e-11
+	
+	for i=1:5
+		temp = x[i] * y[i]
+		if temp < 0
+			push!(neg, temp)
+		else
+			push!(pos, temp)
+		end
+	end
+	
+	
+	pos = sort(pos)
+	neg = sort(neg)
+	posSum = 0
+	negSum = 0
+	
+	for i=length(pos):-1:1
+		posSum += pos[i]
+	end
+	
+	for i=1:length(neg)
+		negSum += neg[i]
+	end
+	
+	sum = posSum + negSum
+	println(sum, " Absolute error: ", abs(value - sum), " Relative error: ", abs(abs(value - sum) / value))
+end
---
+++ List2/Ex1/scalarForward.jl
@@ -0,0 +1,12 @@
+function forwardProduct(type)
+	sum = 0
+	x = type[2.718281828, -3.141592654, 1.414213562, 0.577215664, 0.301029995]
+	y = type[1486.2497, 878366.9879, -22.37492, 4773714.647, 0.000185049]
+	value = -1.00657107e-11
+	
+	for i=1:5
+		sum += x[i] * y[i]
+	end
+	println(sum, " Absolute error: ", abs(value - sum), " Relative error: ", abs(abs(value - sum) / value))
+end
+
