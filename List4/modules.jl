0e73dc9598499458857edc63b004cb00373ee220
Push911 <49253201+Push911@users.noreply.github.com>
2020-11-28 17:24:58 +0100

    Update modules.jl

--- List4/modules.jl
+++ List4/modules.jl
@@ -2,8 +2,10 @@ module modules
     include("Ex1.jl")
     include("Ex2.jl")
     include("Ex3.jl")
+    include("Ex4.jl")
 	
     export ilorazyRoznicowe
     export warNewton
     export naturalna
+    export rysujNnfx
 end
